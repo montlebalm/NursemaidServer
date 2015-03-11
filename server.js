1c0916b8c493d269e31efba293ed701dc7b7f43a
Chris Montrois <cmontrois@quickleft.com>
2015-03-10 19:33:38 -0600

    Allowing process.env port and removing config

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var _ = require('lodash');
-var config = require('config');
 var Hapi = require('hapi');
 var setupAuth = require('./utils/auth-simple');
 
 var server = new Hapi.Server();
-server.connection({port: config.server.port || 3000});
+server.connection({port: process.env.PORT || 3000});
 setupAuth(server);
 
 var VERSIONS = {
