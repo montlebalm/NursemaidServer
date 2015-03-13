788ec25f101d552c742a89fccce0a579b181de1f
Chris Montrois <cmontrois@quickleft.com>
2015-03-12 22:26:19 -0600

    Adding keep-alive

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var _ = require('lodash');
 var Hapi = require('hapi');
+var http = require('http');
 var setupAuth = require('./utils/auth-simple');
 
 var server = new Hapi.Server();
 server.connection({port: process.env.PORT || 3000});
 setupAuth(server);
 
+// Setup versioned api
 var VERSIONS = {
-  "Version 1": "v1"
+  'Version 1': 'v1'
 };
 _.forEach(VERSIONS, function(key) {
   require('./routes/' + key)(key, server);
@@ -15,4 +17,11 @@ _.forEach(VERSIONS, function(key) {
 
 server.start(function () {
   console.log('Server running at:', server.info.uri);
+
+  // Keep-alive
+  if (process.env.API_URL) {
+    setInterval(function() {
+      http.get(process.env.API_URL);
+    }, 600000);
+  }
 });
